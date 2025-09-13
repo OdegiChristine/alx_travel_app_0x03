3f2f92f24f9814d8b7c0c3960cbbb76dbfdc8544
Odegi Christine <cawuor54@gmail.com>
2025-09-13 23:57:05 +0300

    File path changes

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+pip install -r alx_travel_app/requirements.txt
+python alx_travel_app/manage.py migrate --noinput
+python alx_travel_app/manage.py collectstatic --noinput
